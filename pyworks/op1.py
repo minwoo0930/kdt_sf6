f7305288b7d16f1d97c336bd28ae8b0ac32cee39
minwoo0930 <qkrals2519@gmail.com>
2024-08-01 17:17:33 +0900

    파이썬 소스 프로그램

---
+++ pyworks/op1.py
@@ -0,0 +1,35 @@
+# 연산자(operator)
+# 산술 연산 +, -, *, /, //(나누기의 몫), %(나누기의 나머지)
+# 입력
+
+n1 = 10
+n2 = 4
+
+# 산술 연산
+sum = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2 
+
+#출력
+#print(n1, n2)
+print(sum) #14
+print(sub) #6
+print(mul) #40
+print(div) #2.5
+print(n1 // n2) #2
+print(n1 % n2) #2
+
+print(n1 ** n2) #10000
+print(2**3)
+
+
+# 실습
+n3 = 30
+n4 = 4
+
+몫 = n3 // n4
+나머지 = n1 % n2
+
+print(f'빵의 갯수 : {몫}')
+print(f'남은 빵의 갯수 : {나머지}')
---
+++ pyworks/py1.py
@@ -0,0 +1,9 @@
+# 문자를 출력하는 프로그램
+# print() - 출력하는 함수
+print ("hello ~ python")
+
+# 사칙연산 계산 프로그램
+print(1 + 2) #3
+print(1 - 2) #-1
+print(1 * 2) #2
+print(1 / 2) #0.5
